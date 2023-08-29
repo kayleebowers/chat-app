d848443545e51ccb0ede8c56013f0f41b11c4f0b
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-29 18:21:10 +0000

    feat: add flex child elements to App

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
     </View>
   );
 }
@@ -12,7 +15,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     border: "1px solid red"
-  }
+  }, 
+  box1: {
+    backgroundColor: "purple",
+    flex: 4
+  },
+  box2: {
+    backgroundColor: "blue",
+    flex: 4
+  },
+  box3: {
+    backgroundColor: "green",
+    flex: 4
+  },
 });
 
 export default App;
