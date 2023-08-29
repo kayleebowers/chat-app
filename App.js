5a1b99872f6e528a12887bce94b2ddb4d282cadc
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-29 21:03:28 +0000

    feat: add Button to TextInput section

--- App.js
+++ App.js
@@ -1,45 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View, Button, Alert } from "react-native";
+import { useState } from "react";
 
 const App = () => {
   const [text, setText] = useState("");
 
+  // alert users
+  const alertMyText = () => {
+    Alert.alert(text);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.box1}>
-        <TextInput style={styles.textInput} value={text} onChangeText={setText} placeholder="Type something here"></TextInput>
-        <Text style={styles.textInput}>You typed: {text}</Text>
-      </View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder="Type something here"
+      ></TextInput>
+      <Text style={styles.textInput}>You typed: {text}</Text>
+      <Button
+        title="Save"
+        onPress={() => {
+          alertMyText();
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    border: "1px solid red"
-  }, 
+    border: "1px solid red",
+  },
   box1: {
     backgroundColor: "purple",
-    flex: 4
+    flex: 4,
   },
   box2: {
     backgroundColor: "blue",
-    flex: 4
+    flex: 4,
   },
   box3: {
     backgroundColor: "green",
-    flex: 4
+    flex: 4,
   },
   textInput: {
-    width: '88%',
+    width: "88%",
     borderWidth: 1,
     height: 50,
     padding: 10,
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
 });
 
