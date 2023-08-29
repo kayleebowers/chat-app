6959f690980df693c361958ad2d43a44adcf3d35
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-29 21:25:05 +0000

    feat: add Screen1 and 2 components, import in App

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 import { StyleSheet, Text, TextInput, View, Button, Alert, TouchableOpacity, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Screen1 from "./components/Screen1";
+import Screen2 from "./components/Screen2";
 
 const App = () => {
 
