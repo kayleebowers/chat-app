1444ba81e8b90f1294f3d714f4807d1d49aa810f
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-30 16:23:08 +0000

    feat: create navigator between screen 1 and 2

--- App.js
+++ App.js
@@ -5,10 +5,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Screen1 from "./components/Screen1";
 import Screen2 from "./components/Screen2";
 
+// create navigator to return object with Navigator and Screen components
+const Stack = createNativeStackNavigator();
+
 const App = () => {
 
   return (
-    <View></View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen1">
+        <Stack.Screen name="Screen1" component={Screen1}/>
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
