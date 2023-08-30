4854993401cea80ce9aeddc35c714dd8d5bdc6bf
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-30 16:54:14 +0000

    feat: track username state, pass data to Screen2

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 const Screen1 = ({ navigation }) => {
+  const [name, setName] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Hello Screen1</Text>
+      <TextInput
+        value={name}
+        onChangeText={(name) => setName(name)}
+        style={styles.textInput}
+        placeholder="Type username here"
+      />
       <Button
         title="Go to Screen2"
-        onPress={() => navigation.navigate("Screen2")}
+        // pass name data to Screen2 on screen transition
+        onPress={() => navigation.navigate("Screen2", {name: name})}
       ></Button>
     </View>
   );
@@ -17,6 +27,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  textInput: {
+    width: "88%",
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 15,
   },
 });
 
