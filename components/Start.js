7936ecad213e1936c6669ae6f29339ca7b57b5c0
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-31 16:03:41 +0000

    style: wrap color selection section in view

--- components/Start.js
+++ components/Start.js
@@ -25,12 +25,14 @@ const Start = ({ navigation }) => {
           style={styles.textInput}
           placeholder="Type username here"
         />
-        <Text>Choose background color:</Text>
-        <View style={styles.colorOptions}>
-          <TouchableOpacity style={[styles.colorButton, styles.black]}></TouchableOpacity>
-          <TouchableOpacity style={[styles.colorButton, styles.purple]}></TouchableOpacity>
-          <TouchableOpacity style={[styles.colorButton, styles.blue]}></TouchableOpacity>
-          <TouchableOpacity style={[styles.colorButton, styles.green]}></TouchableOpacity>
+        <View style={styles.bgColorSection}>
+          <Text>Choose background color:</Text>
+          <View style={styles.colorOptions}>
+            <TouchableOpacity style={[styles.colorButton, styles.black]}></TouchableOpacity>
+            <TouchableOpacity style={[styles.colorButton, styles.purple]}></TouchableOpacity>
+            <TouchableOpacity style={[styles.colorButton, styles.blue]}></TouchableOpacity>
+            <TouchableOpacity style={[styles.colorButton, styles.green]}></TouchableOpacity>
+          </View>
         </View>
         <Button
           title="Start Chatting"
@@ -68,7 +70,8 @@ const styles = StyleSheet.create({
     height: "44%",
     width: "88%",
     top: "22%",
-    justifyContent: "space-evenly"
+    justifyContent: "space-between",
+    padding: "5%",
   },
   startChattingButton: {
     fontSize: 16,
