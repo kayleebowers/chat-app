c2b4ffe9b39715d410711f2022f74785b510c029
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-07 21:11:17 +0000

    build: configure Firebase and initialize Firestore

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, TextInput, View, Button, Alert, TouchableOpacity, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { initializeApp } from "firebase/app";
+import { getFirestore as getRestore } from "firebase/firestore";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
@@ -9,6 +10,22 @@ import Chat from "./components/Chat";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  // configure Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyDj2n6B8QR6Cw6mt4_9jnY0W6DgFGp0Nl0",
+    authDomain: "chat-app-35ebf.firebaseapp.com",
+    projectId: "chat-app-35ebf",
+    storageBucket: "chat-app-35ebf.appspot.com",
+    messagingSenderId: "820030470933",
+    appId: "1:820030470933:web:1faf8f6b9706ee6759871c"
+  };
+
+  // initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // initialize Cloud Firestore
+  const db = getRestore(app);
 
   return (
     <NavigationContainer>
